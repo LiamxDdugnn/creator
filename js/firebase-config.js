fc6035b381805c5b7c0b53039e68db76ff7ac99c
phạm tiến dũng <tdungx02@gmail.com>
2025-06-02 13:53:58 +0700

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,22 +1,18 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
-import {
-  getFirestore, collection, addDoc
-} from "https://www.gstatic.com/firebasejs/9.22.2/firebase-firestore.js";
-import {
-  getStorage, ref, uploadBytes, getDownloadURL
-} from "https://www.gstatic.com/firebasejs/9.22.2/firebase-storage.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDe3-vuNDGJ_DxXjRV4kX91cvUy9EXwFFo",
-  authDomain: "dearlove-demo.firebaseapp.com",
-  projectId: "dearlove-demo",
-  storageBucket: "dearlove-demo.appspot.com",
-  messagingSenderId: "469241383255",
-  appId: "1:469241383255:web:bc4e7e6f73e12aa1375d59"
+  apiKey: "AIzaSyBM98U7dEV8jvUQ0nmw_GqG4gmI7UPuCGU",
+  authDomain: "dearlove-spark.firebaseapp.com",
+  projectId: "dearlove-spark",
+  storageBucket: "dearlove-spark.appspot.com",
+  messagingSenderId: "98130036485",
+  appId: "1:98130036485:web:548c3526f4e93109ed9f2b"
 };
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const storage = getStorage(app);
 
-export { db, collection, addDoc, storage, ref, uploadBytes, getDownloadURL };
+export { db, storage };
